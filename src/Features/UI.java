d07aef59346ae38ea6bd9fa4a1242452ba13002e
SparkyCloudy <satriagama1@gmail.com>
2023-01-12 00:16:16 +0800

    Revamp UI (1)

--- src/Features/UI.java
+++ src/Features/UI.java
@@ -1,27 +1,87 @@
 package Features;
 
-public class UI extends Form {
+import Features.Management.*;
+import java.util.Scanner;
+
+public class UI {
+    static Checker check = new Checker();
+    Scanner input = new Scanner(System.in);
     public static void initUI() {
+        var menu = new UI();
+        menu.initMainMenu();
+    }
+
+
+    private void initMainMenu() {
         while (true) {
-            System.out.println("1. Register Mahasiswa");
-            System.out.println("2. Check NIM Mahasiswa");
+            System.out.println("1. Menu Mahasiswa");
+            System.out.println("2. Menu Buku");
             System.out.println("3. Exit Program");
             System.out.print("Pilihan: ");
 
             String choice = input.nextLine();
-            if (check.isNumber(choice)) {
-                switch (Integer.parseInt(choice)) {
-                    case 1 -> initRegister();
-                    case 2 -> initChecker();
-                    case 3 -> {
-                        return;
-                    }
-                    default -> System.out.println("Pilihan yang dimasukan salah!");
-                }
-            } else {
-                System.out.println("Pilihan yang dimasukan salah!");
+            if (!check.isNumber(choice)) {
+                System.out.println("Harus berupa angka dan tidak boleh kosong!");
+                continue;
             }
 
+            switch (Integer.parseInt(choice)) {
+                case 1 -> menuMahasiswa();
+                case 2 -> menuBuku();
+                case 3 -> {
+                    return;
+                }
+                default -> System.out.println("Pilihan yang dimasukan salah!");
+            }
+        }
+    }
+
+    private void menuMahasiswa() {
+        while (true) {
+            System.out.println("1. Register Mahasiswa");
+            System.out.println("2. Check NIM Mahasiswa");
+            System.out.println("3. Kembali");
+            System.out.print("Pilihan: ");
+
+            String choice = input.nextLine();
+            if (!check.isNumber(choice)) {
+                System.out.println("Harus berupa angka dan tidak boleh kosong!");
+                continue;
+            }
+
+            switch (Integer.parseInt(choice)) {
+                case 1 -> Form.initRegister();
+                case 2 -> Form.initChecker();
+                case 3 -> {
+                    return;
+                }
+                default -> System.out.println("Pilihan yang dimasukan salah!");
+            }
+        }
+    }
+
+    private void menuBuku() {
+        while (true) {
+            System.out.println("1. Donasi Buku");
+            System.out.println("2. Pinjam Buku");
+            System.out.println("3. Cari Buku");
+            System.out.println("4. Kembali");
+            System.out.print("Pilihan: ");
+
+            String choice = input.nextLine();
+            if (!check.isNumber(choice)) {
+                System.out.println("Harus berupa angka dan tidak boleh kosong!");
+                continue;
+            }
+            switch (Integer.parseInt(choice)) {
+                case 1 -> Book.initDonation();
+                case 2 -> Book.initBorrow();
+                case 3 -> Book.initSearch();
+                case 4 -> {
+                    return;
+                }
+                default -> System.out.println("Pilihan yang dimasukan salah!");
+            }
         }
     }
 }
