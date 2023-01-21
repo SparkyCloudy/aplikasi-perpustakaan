370baa8fbc671e00712224c392255d0e8da149e7
SparkyCloudy <satriagama1@gmail.com>
2023-01-21 12:21:10 +0800

    Added initSearch() method

--- src/Features/Book.java
+++ src/Features/Book.java
@@ -31,6 +31,7 @@ class Book {
     final static String loanpath = env.get("LOANJSON");
 
     public static void initDonation() {
+        check.clearConsole();
         var obj = new JsonObject();
         temp.clear();
 
@@ -61,6 +62,7 @@ class Book {
                 initDonation();
             } else {
                 System.out.println("Kombinasi tidak ditemukan!");
+                check.clearConsole(2);
                 initDonation();
             }
 
@@ -73,6 +75,7 @@ class Book {
         temp.add(value);
 
         while (true) {
+            check.clearConsole();
             System.out.printf("Masukan Nama donatur buku %s. %n", temp.get(0));
             System.out.println("Back: ^B");
             System.out.println("Cancel: ^C");
@@ -115,6 +118,7 @@ class Book {
         }
 
         while (true) {
+            check.clearConsole();
             System.out.printf("Masukan Jumlah buku \"%s\" yang didonasikan. %n", temp.get(0));
             System.out.println("Back: ^B");
             System.out.println("Cancel: ^C");
@@ -155,6 +159,7 @@ class Book {
 
         // Confirmation prompt
         while (true) {
+            check.clearConsole();
             System.out.println("[KONFIRMASI] Apakah detail buku dibawah sudah tepat?");
             System.out.println("Nama Buku\t : " + temp.get(0));
             System.out.println("Nama Donatur : " + temp.get(1));
@@ -191,6 +196,7 @@ class Book {
 
         // Write the list to the JSON file
         check.writeListToFile(list, bookpath);
+        check.clearConsole();
     }
 
     public static void initBorrow() {
@@ -199,6 +205,7 @@ class Book {
         var jArray = new JsonArray();
         var list = check.getDatabaseList(userpath);
         while (true) {
+            check.clearConsole();
             System.out.println("Masukan NIM mahasiswa yang ingin meminjam buku");
             System.out.println("Back: ^B");
             System.out.println("Cancel: ^C");
@@ -220,12 +227,14 @@ class Book {
                     return;
                 } else {
                     System.out.println("Kombinasi tidak ditemukan!");
+                    check.clearConsole(3);
                     continue;
                 }
             }
 
             if (!check.elementIsValid(value, list, "nim")) {
                 System.out.println("NIM Mahasiswa tidak ditemukan, harap mendaftarkan NIM tersebut");
+                check.clearConsole(3);
                 continue;
             }
 
@@ -236,6 +245,7 @@ class Book {
 
         list = check.getDatabaseList(bookpath);
         while (true) {
+            check.clearConsole();
             System.out.println("List buku yang tersedia di Perpustakaan.");
             for (int i = 0; i < list.size(); i++) {
                 var element = list.get(i);
@@ -252,6 +262,7 @@ class Book {
 
             if (value.isEmpty()) {
                 System.out.println("Input tidak boleh kosong!");
+                check.clearConsole(3);
                 continue;
             }
 
@@ -274,36 +285,92 @@ class Book {
                     return;
                 } else {
                     System.out.println("Kombinasi tidak ditemukan!");
+                    check.clearConsole(3);
                     continue;
                 }
             }
 
             var isNumber = check.isNumber(value);
-            if (isNumber) {
-                // Get JSON Element on x index
-                var element = list.get(Integer.parseInt(value) - 1);
+            if (!isNumber) {
+                System.out.println("Buku/Pilihan tidak ditemukan.!");
+                continue;
+            }
 
-                // Get JSON Property value and parse it as String
-                value = element.getAsJsonObject().get("bookName").getAsString();
-
-                // Add the string into the JsonArray
+            var isString = check.isString(value);
+            if (isString && check.elementIsValid(value, list, "bookName")) {
+                var element = list.getAsJsonObject().get("bookName");
+                value = element.getAsString();
                 jArray.add(value);
                 continue;
             }
 
-            // var isString = check.isString(value);
-            /*if (isString) {
+            // Get JSON Element on x index
+            var element = list.get(Integer.parseInt(value) - 1);
 
-            }*/
+            // Get JSON Property value and parse it as String
+            value = element.getAsJsonObject().get("bookName").getAsString();
 
-            System.out.println("Buku/Pilihan tidak ditemukan.!");
+            // Add the string into the JsonArray
+            jArray.add(value);
         }
 
         list = check.addObjectToList(obj, loanpath);
         check.writeListToFile(list, loanpath);
+        check.clearConsole();
     }
 
     public static void initSearch() {
+        check.clearConsole();
+        while (true) {
+            var list = check.getDatabaseList(loanpath);
 
+            if (list == null) {
+                System.out.println("Database kosong, harap diisi terlebih dahulu menggunakan menu \"Donasi Buku\".");
+                check.clearConsole(3);
+                break;
+            }
+
+            System.out.println("List Mahasiswa/i yang meminjam di Perpustakaan.");
+            for (int i = 0; i < list.size(); i++) {
+                var element = list.get(i);
+                var name = element.getAsJsonObject().get("nim").getAsString();
+                System.out.printf("%s. %s %n", (i+1), name);
+            }
+            System.out.println("================");
+            System.out.println("Back: ^B");
+            System.out.print(">> ");
+
+            var value = input.nextLine();
+
+            var length = value.length();
+            if (value.startsWith("^") && length == 2) {
+                if (value.contains("B")) {
+                    // Back to last menu
+                    return;
+                } else {
+                    System.out.println("Kombinasi tidak ditemukan!");
+                    check.clearConsole(3);
+                    continue;
+                }
+            }
+
+            var isNumber = check.isNumber(value);
+            if (!isNumber) {
+                System.out.println("Input harus berupa angka dan tanpa spasi!.");
+                check.clearConsole(3);
+                continue;
+            }
+
+            check.clearConsole();
+            var element = list.get(Integer.parseInt(value) - 1);
+            var jArray = element.getAsJsonObject().get("book").getAsJsonArray();
+            var name = element.getAsJsonObject().get("nim").getAsString();
+            System.out.printf("List buku yang dipinjam oleh %s. %n", name);
+            for (int i = 0; i < jArray.size(); i++) {
+                value = jArray.get(i).getAsString();
+                System.out.printf("%d. %s %n%n", (i+1), value);
+            }
+            System.out.println();
+        }
     }
 }
