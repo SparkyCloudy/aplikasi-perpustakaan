e6c64742f5f36c39ac4306efa9506e462cfed32b
SparkyCloudy <satriagama1@gmail.com>
2023-01-23 02:26:11 +0800

    Added environment checker

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,21 @@
 import Features.*;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class Main {
+    // Environment Variables
+    static Dotenv env = Dotenv.load();
+    final static String bookpath = env.get("BOOKJSON");
+    final static String userpath = env.get("USERJSON");
+    final static String loanpath = env.get("LOANJSON");
+
     public static void main(String[] args) {
         var program = new Main();
+        if (bookpath != null && userpath != null && loanpath != null) {
+            program.initModule();
+        } else {
+            System.out.println("Environtment variables for Database not ready, please read \".env.example\" file!");
+        }
 
-        program.initModule();
     }
 
     void initModule() {
