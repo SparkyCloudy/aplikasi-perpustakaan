ec2d52c8b2eba1b9732eea7e73810bbf406ce9f8
SparkyCloudy <satriagama1@gmail.com>
2023-01-23 02:49:13 +0800

    Simplified build script

--- build.bat
+++ build.bat
@@ -1,14 +1,12 @@
 @echo off
 
-set build=%JAVA_HOME%\bin\javac
-set run=%JAVA_HOME%\bin\java
 mkdir out
 
 rem Compile all Java source files
 xcopy /y "src\.env" out
-%build% -d out/ -cp lib/*;src src/*.java
+javac -d out/ -cp lib/*;src src/*.java
 
 rem Run the main class
-%run% -cp lib/*;out Main
+java -cp lib/*;out Main
 
 @exit
